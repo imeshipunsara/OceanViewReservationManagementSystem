579968a78ce7611ec759dd60d321cdc40e34e2f4
Imeshi Punsara <imeshipunsara9@gmail.com>
2026-03-06 09:02:36 +0530

    update bill adding extra charge

---
+++ src/main/java/com/oceanview/model/ExtraCharge.java
@@ -0,0 +1,63 @@
+package com.oceanview.model;
+
+import java.math.BigDecimal;
+
+public class ExtraCharge {
+    private int chargeId;
+    private int billId;
+    private String itemName;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal subtotal;
+
+    public ExtraCharge() {
+    }
+
+    public int getChargeId() {
+        return chargeId;
+    }
+
+    public void setChargeId(int chargeId) {
+        this.chargeId = chargeId;
+    }
+
+    public int getBillId() {
+        return billId;
+    }
+
+    public void setBillId(int billId) {
+        this.billId = billId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+}
